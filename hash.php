16aae349a467e7de62e2fa3a20955f8d9d954c99
sid <siddhant.17dash@gmail.com>
2020-05-13 13:11:06 +0530

    adding sha1 cracker

--- hash.php
+++ hash.php
@@ -1,37 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hash cracker</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="hash.php">----Hash cracker----</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
 
-<html>
-<head>
-  <title>Hash cracker</title>
+      </div>
+    </nav>
 
-  <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-
-<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
-
-</head>
-
-
-<body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="hash.php">----Hash cracker----</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-
-    </div>
-  </nav>
-
-<p>Hash cracker is a tool for hash cracking by comparing it with stored common passwords. <br>
-<<<<<<<<<<<<-------------Version : 1.1 ----------------------->>>>>>>>
+<p>HAsh cracker is a tool for hash cracking by comapring it with stored common passwords. <br>
+<<<<<<<<<<<<<<<<<------------------version : 1.2------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>
 </p>
 
-<form action="logic.php" method="post">
-  Enter hash:<input type="text" name="hash"   > <br>
-  <button type="submit" class="btn btn-default">Submit</button>
+<form  action="logic.php" method="post">
+  Enter (MD5 or SHA1)hash:<input type="text" name="hash" > <br>
+  <button type="submit" name="button">Submit</button>
 </form>
+
+
+
+  </body>
+</html>
--- logic.php
+++ logic.php
@@ -1,22 +1,70 @@
 <?php
 
-$hash=$_POST["hash"];
+$hash= $_POST["hash"];
 $hash= strtolower($hash);
 
-$handle = fopen("password.txt", "r");
-if ($handle) {
-    while (($line = fgets($handle)) !== false) {
-        // process the line read.
-        $hash_line= md5(trim($line));
-        if($hash_line === $hash){
-          echo "Password cracked! <br>";
-          echo "Cracked string:".$line;
+function formd5($hash){
+echo "md5 hash identified!!<br>";
+$handle= fopen("password.txt","r");
 
-        }
+if($handle){
+  while (($line = fgets($handle)) !== false){
+    $hash_line= md5(trim($line));
+    if($hash_line === $hash){
+      echo "PAssword cracked !! <br>";
+      echo "Cracked string:". $line;
     }
-
-    }
-
-else{
-  echo "file error";
+  }
 }
+else {
+  echo "error file opening!!";
+}
+
+}
+
+function forsha1($hash){
+  echo "sha1 hash identified!<br>";
+  $handle= fopen("password.txt","r");
+
+  if($handle){
+    while (($line = fgets($handle)) !== false){
+      $hash_line= sha1(trim($line));
+      if($hash_line === $hash){
+        echo "PAssword cracked !! <br>";
+        echo "Cracked string:". $line;
+      }
+    }
+  }
+  else {
+    echo "error file opening!!";
+  }
+}
+
+if(strlen($hash)==32){
+  formd5($hash);
+}
+elseif (strlen($hash)==40) {
+  forsha1($hash);
+}
+else {
+  echo "Bad input// coulnt identify md5 or sha1 hash---";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
